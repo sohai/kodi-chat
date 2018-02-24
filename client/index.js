0699b33152eacd27ddaf092d559a6c620ff0509c
= <=>
2018-02-24 19:51:06 +0100

    Basic styles and html

--- client/index.js
+++ client/index.js
@@ -2,6 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const App = () => <h1>Kodify chat</h1>;
+const App = () => (
+  <div className="root">
+    <div className="app-bar">
+      <h1>Kodify Chat</h1>
+    </div>
+    <div className="content">
+      <div className="message outgoing">Messages</div>
+      <div className="message incoming">Messages1</div>
+      <div className="message outgoing">Messages2ge </div>
+    </div>
+    <div className="footer">
+      <input type="text" placeholder="Type here..." className="input" />
+    </div>
+  </div>
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
