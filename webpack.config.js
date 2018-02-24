391542f6f7901295792f9e720d5d208a86782177
= <=>
2018-02-24 19:01:59 +0100

    Webpack suppor loading css

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,10 @@
 const path = require('path');
 const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const config = {
   entry: {
-    vendor: [
-      'babel-polyfill',
-      'react',
-      'react-dom'
-    ],
+    vendor: ['babel-polyfill', 'react', 'react-dom'],
     app: ['./client/index.js']
   },
   output: {
@@ -15,21 +12,31 @@ const config = {
     filename: '[name].js'
   },
   module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['react', 'env', 'stage-2']
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['react', 'env', 'stage-2']
+          }
         }
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader'
+        })
       }
-    }]
+    ]
   },
   plugins: [
     new webpack.optimize.CommonsChunkPlugin({
       name: 'vendor'
-    })
+    }),
+    new ExtractTextPlugin('app.css')
   ]
 };
 
